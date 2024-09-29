7adade1b215df5daef2e37c296d5601e15a32e68
git22202 <git22202@gmail.com>
2024-09-29 13:49:32 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valuInNumber = Number(score)
+// console.log(typeof valuInNumber);
+// console.log(valuInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Ayush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ayush" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
