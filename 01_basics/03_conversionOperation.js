f71556d2aac794910269b3488e43777b0b1fbeef
git22202 <git22202@gmail.com>
2024-09-29 14:04:43 +0000

    conversion datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,7 +16,7 @@ let valuInNumber = Number(score)
 let isLoggedIn = "Ayush"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -26,10 +26,44 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+//****************** Operation ******************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
 
+let s1 = "Hello"
+let s2 = " Ayush"
+let s3 = s1 + s2
+
+// console.log(s3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+// console.log(num3);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
