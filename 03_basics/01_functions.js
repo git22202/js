efbe8e795949142c0512f13220289800ed88b6b8
git22202 <git22202@gmail.com>
2024-10-05 03:38:06 +0000

    functions end

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,6 +29,35 @@ function loginUserMessage(username = "Sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Ayush"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("Ayush"));
+// console.log(loginUserMessage());
 
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 300, 400, 2000));
+
+const user = {
+    username: "Ayush",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 199
+})
+
+const myNewArray = [100, 200, 300, 400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue([100, 200, 300, 400]));
