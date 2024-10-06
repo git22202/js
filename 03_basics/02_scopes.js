32b9b8cd45b572031f3455ee658072509a83dc44
git22202 <git22202@gmail.com>
2024-10-06 01:06:46 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,11 +2,47 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username = "Ayush"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Ayush"
+    if (username === "Ayush") {
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
